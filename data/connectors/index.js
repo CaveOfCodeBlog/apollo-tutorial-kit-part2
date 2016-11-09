523ba49b69f0edf92d62dfba6a92f8f0e948d0c2
Jesper O. Christensen <karnich99@gmail.com>
2016-11-09 23:04:33 +0100

    mongoose schemas added

--- data/connectors/index.js
+++ data/connectors/index.js
@@ -1,6 +1,24 @@
-import * as Mongoose from "mongoose";
+import * as Mongoose from 'mongoose';
 
-export const MongooseConnection = Mongoose.connect("mongodb://localhost:27017/test")
+const CarSchema = new Mongoose.Schema({
+  model: String,
+  registrationNo: String,
+  owner: Mongoose.Schema.Types.ObjectId,
+});
+
+const UserSchema = new Mongoose.Schema({
+  displayName: String,
+  email: String,
+  firstName: String,
+  lastName: String,
+  birthday: String,
+});
+
+export const User = Mongoose.model('User', UserSchema);
+export const Car = Mongoose.model('Car', CarSchema);
+
+
+export const MongooseConnection = Mongoose.connect('mongodb://localhost:27017/test')
   .catch((connectError) => {
-    console.error("Could not connect to MongoDB on port 27017", connectError);
+    console.error('Could not connect to MongoDB on port 27017', connectError);
   });
